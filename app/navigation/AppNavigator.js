a1dddf7bbd4d5ae7ce33707c052b0ed6e46541b3
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2021-11-19 21:10:00 +0300

    Added material community icons to app navigator

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import { HomeScreen, AccountScreen } from '../screens';
 
 function SettingsScreen() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Settings!</Text>
+            <Text>Add Listings page</Text>
         </View>
     );
 }
@@ -17,9 +18,27 @@ const Tab = createMaterialBottomTabNavigator();
 export default function App() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Settings" component={SettingsScreen} />
-            <Tab.Screen name="Account" component={AccountScreen} />
+            <Tab.Screen
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="home" color={color} size={26} />
+                    ),
+                }}
+                name="Home" component={HomeScreen} />
+            <Tab.Screen
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="plus-box" color={color} size={26} />
+                    ),
+                }}
+                name="Listing" component={SettingsScreen} />
+            <Tab.Screen
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="account" color={color} size={26} />
+                    ),
+                }}
+                name="Account" component={AccountScreen} />
         </Tab.Navigator>
     );
 }
